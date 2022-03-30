ae8a381d786ad6c1a4cdbbd38d846a3db8c7db25
Jason Zhang <jasonjzhang17@gmail.com>
2022-03-30 09:24:02 -0400

    Changed channel member_ids to set

--- models/channel.py
+++ models/channel.py
@@ -1,13 +1,11 @@
 from models.message import Message
 from models.user import User
 
-import collections
-
 class Channel:
     def __init__(self):
         #Attributes
         self.private = False
-        self.member_ids = []
+        self.member_ids = set()
         self.banned = []
         self.roles = {} #{owners:[user1], administrators:[user5, user8], etc}
         self.messages = []
@@ -17,7 +15,4 @@ class DirectChannel(Channel):
     def __init__(self, user_1: User, user_2: User):
         super().__init__()
         self.private = True
-        self.member_ids = [user_1['user_id'], user_2['user_id']]
-
-    def sorted_member_ids(self):
-        return collections.sorted(self.member_ids)
+        self.member_ids.update([user_1['user_id'], user_2['user_id']])
