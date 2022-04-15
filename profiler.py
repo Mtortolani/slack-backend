93b5817487344db420f89773123b6aae5b3e17eb
Jason Zhang <jasonjzhang17@gmail.com>
2022-04-15 00:08:31 -0400

    Updated profiling code

--- profiler.py
+++ profiler.py
@@ -1,16 +1,22 @@
-from re import S
 import time
 import matplotlib.pyplot as plt
 
 run_times = []
 
-for i in range(50):
-    start_time = time.process_time()
-    for _ in range(10000):
-        print(_)
+# for i in range(20):
+#     start_time = time.process_time()
+#     time.sleep(1)
+#     # print(time.process_time() - start_time)
+#     # print(start_time)
+#     run_times.append(time.process_time() - start_time)
+
+for i in range(10):
+    start_time = time.time()
+    time.sleep(1)
+    print('running')
     # print(time.process_time() - start_time)
     # print(start_time)
-    run_times.append(time.process_time() - start_time)
+    run_times.append(time.time() - start_time)
 
 print(run_times)
 plt.hist(run_times)
