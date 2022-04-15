b7d0db3c00738751461a6dbced0e1dcd7074a9c9
Jason Zhang <jasonjzhang17@gmail.com>
2022-04-14 23:51:04 -0400

    Created plot from profiling

--- profiler.py
+++ profiler.py
@@ -1,8 +1,17 @@
 from re import S
 import time
+import matplotlib.pyplot as plt
 
-start_time = time.process_time()
-for _ in range(200000):
-    print(_)
-print(time.process_time() - start_time)
-print(start_time)
+run_times = []
+
+for i in range(50):
+    start_time = time.process_time()
+    for _ in range(10000):
+        print(_)
+    # print(time.process_time() - start_time)
+    # print(start_time)
+    run_times.append(time.process_time() - start_time)
+
+print(run_times)
+plt.hist(run_times)
+plt.show()
