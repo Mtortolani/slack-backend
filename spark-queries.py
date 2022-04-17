f5a6b8fdd529e37364dc34a613d9718735810b55
Marco tortolani <tortolani.mar@northeastern.edu>
2022-04-16 20:01:09 -0400

    spark queries

--- spark-queries.py
+++ spark-queries.py
@@ -4,6 +4,8 @@ from operator import __add__
 from pyspark.sql import SparkSession
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+from pyspark.sql.functions import *
+from mongo_queries import workspaceByUser
 
 class SparkQuery:
     def __init__(self, database: str = 'slack_database', col: str = 'user_col'):
@@ -29,29 +31,63 @@ class SparkQuery:
         df = self.spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
         df.printSchema()
     
+    # utilize SQLContext to run sql queries on mongo df
+    # run generic pull all query
     def showAllData(self):
         df = self.spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
         df.createOrReplaceTempView('user_col')
         df =self.sqlC.sql('SELECT * FROM user_col')
         df.show()
 
-    def randomUserIds(self,count:int = 1) -> list:
-        # utilize SQLContext to run sql queries on mongo df
-        # run generic pull all query
-        df = self.spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+    def randomUserIds(self, count:int = 1) -> list:
+        # pick a random user(s)
+        df = self.spark.read\
+            .option("database", self.database)\
+                .option("collection", "user_col")\
+                    .format("com.mongodb.spark.sql.DefaultSource").load()
         df.createOrReplaceTempView('user_col')
         df = self.sqlC.sql(f'''SELECT user_id FROM user_col
                     Order By RAND()
                     LIMIT {count}''')
-        df.show()
-        user_id = [id for id in df.collect()[0]]
+        user_id = [df.collect()[i][0] for i in range(df.count())]
         return user_id
+    
+    # find all workspaces a user is in
+    def workspaceByUser(self, user_id: int) -> list:
+        df = self.spark.read\
+                .option("database", self.database)\
+                    .option("collection", "workspace_col")\
+                        .format("com.mongodb.spark.sql.DefaultSource").load()
+        df.createOrReplaceTempView('workspace_col')
+        df = self.sqlC.sql(f'''select * from workspace_col 
+                           where array_contains(members, {user_id})''')
+        df.show()
+        workspace_ids = []
+        for row in df.collect():
+            workspace_ids.append(row[0][0])
+        return workspace_ids
 
+
+    # find all messages in direct channel between two users
+    # will return empty list if no direct channel between two users exists
+    def dirChannelMessages(self, user_id_1: int, user_id_2: int):
+        df = self.spark.read\
+                .option("database", self.database)\
+                    .option("collection", "direct_col")\
+                        .format("com.mongodb.spark.sql.DefaultSource").load()
+        df = self.sqlC.sql(f'''select * from direct_col 
+                           where array_contains(members, {user_id_1})
+                           AND array_contains(members, {user_id_2}''')
+        df.filter(array_contains('member_ids',[user_id_1,user_id_2])) 
+                       
 def main():
     SQ = SparkQuery('slack_database','user_col')
     SQ.checkSchema()
-    print(SQ.randomUserIds())
-    
+    randomId = SQ.randomUserIds(10)
+    print(randomId)
+    workspacesForUser = SQ.workspaceByUser(randomId[0])
+    print(workspacesForUser)
+    dir_channels = SQ.dirChannelMessages(randomId[0], randomId[1])
 
 if __name__ == '__main__':
     main()
